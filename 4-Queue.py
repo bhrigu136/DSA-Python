489a81b05c8e854bae35e39d623a2b1ab0e83cf6
Tamanna Bhrigunath <tamanna900620@gmail.com>
2026-01-13 00:21:30 +0530

    Add dequeue implementation, update queue, and rename palindrome solution file

--- 4-Queue.py
+++ 4-Queue.py
@@ -5,7 +5,6 @@ Types -
 - Circular Queue
 - Priority Queue
 
-
 ## Basic Operations
 Enqueue (insert) -> rear -> O(1)
 Dequeue (delete) -> front -> O(1)
@@ -13,8 +12,38 @@ Front/Peek -> View the first element -> O(1)
 isEmpty -> Check if queue is empty
 isFull -> Check if queue is full (for array-based queue)
 
-
 ### Queue Overflow & Underflow
 Overflow: Inserting when queue is full
 Underflow: Deleting when queue is empty
+Note: In python, we don't need to check 
+
 """
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return len(self.items)== 0
+    
+    def insert(self, value):
+        self.items.append(value)
+
+    def delete(self):
+        if(self.isEmpty()):
+            print("Queue is Empty")
+        else:
+            return self.items.pop(0)
+        
+q = Queue()
+q.insert(10)
+q.insert(20)
+q.insert(30)
+q.insert(40)
+
+print(q.delete()) #10
+print(q.delete()) #20
+print(q.delete()) #30
+print(q.delete()) #40
+print(q.delete()) # Queue is Empty
+        
---
+++ 4.1-Dequeue.py
@@ -0,0 +1,43 @@
+class Deque:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return len(self.items)== 0
+    
+    def insertAtEnd(self, value):
+        self.items.append(value)
+
+    def deleteAtFront(self):
+        if(self.isEmpty()):
+            print("Queue is Empty")
+        else:
+            return self.items.pop(0)
+        
+    def insertAtFront(self, value):
+        self.items.insert(0, value)
+
+    def deleteAtEnd(self):
+        if(self.isEmpty()):
+            print("Queue is Empty")
+        else:
+            return self.items.pop()
+
+dq = Deque()
+dq.insertAtEnd(10)
+dq.insertAtFront(20)
+dq.insertAtEnd(30)
+dq.insertAtEnd(40)
+dq.insertAtFront(50)
+print(dq.deleteAtEnd()) #40
+print(dq.deleteAtEnd()) #30
+print(dq.deleteAtFront()) #50
+print(dq.deleteAtFront()) #20
+print(dq.deleteAtFront()) #10
+print(dq.deleteAtFront()) #Queue is Empty
+print(dq.deleteAtEnd()) #Queue is Empty
+
+
+
+
+        
