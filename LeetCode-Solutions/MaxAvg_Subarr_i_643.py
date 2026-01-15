5f646fe90aa8f2aa3b4c934f95f0a6a0e6c4e09a
Tamanna Bhrigunath <tamanna900620@gmail.com>
2026-01-15 18:55:24 +0530

    LeetCode - 643 - Maximum Average Subarray I

---
+++ LeetCode-Solutions/MaxAvg_Subarr_i_643.py
@@ -0,0 +1,18 @@
+def find_max_avg(nums, k):
+    n = len(nums)
+    current_sum = sum(nums[:k])
+    max_sum = current_sum
+
+    for i in range(k, n):
+        current_sum += nums[i] - nums[i - k]
+        max_sum = max(max_sum, current_sum)
+    return max_sum / k
+
+if __name__ == "__main__":
+    n = int(input("Enter the size of the array: "))
+
+    print("Enter the array elements: ")
+    nums = list(map(int, input().split()))
+    k = int(input("Enter the size of the subarray (k): "))
+    result = find_max_avg(nums, k)
+    print(f"The maximum average of subarrays of size {k} is: {result}")
