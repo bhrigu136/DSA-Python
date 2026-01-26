3fbca096fc46b5727ef44e5b82c079edec205647
Tamanna Bhrigunath <tamanna900620@gmail.com>
2026-01-26 08:59:42 +0530

    added searching and deleting in BST

--- BST_8.py
+++ BST_8.py
@@ -14,30 +14,72 @@ def insert(root, value):
     else:
         root.right = insert(root.right, value)
     return root
-    
+
+def search(root, value):
+    if root == None:
+        print("Value not found in the BST.", end = '\n')
+        return
+    if root.value == value:
+        print("Value found in the BST.", end = '\n')
+        return
+    if value < root.value:
+        root.left = search(root.left, value)
+    else:
+        root.right = search(root.right, value)
+    return root
+
+def get_successor(node):
+    current = node.right    
+    while current != None and current.left != None:
+        current = current.left
+    return current
+
+
+def delete(root, value):
+    if root == None:
+        return root
+    if root.value > value:
+        root.left = delete(root.left, value)
+    elif root.value < value:
+        root.right = delete(root.right, value)
+    else:
+        if root.left == None:
+            return root.right
+        if root.right == None:
+            return root.left
+        else:
+            successor = get_successor(root)
+            root.value = successor.value
+            root.right = delete(root.right, successor.value)
+    return root
+
 def inorder(root):
     if root != None:
         inorder(root.left)
         print(root.value, end = ' ')
         inorder(root.right)
             
-root = insert(None, 20)
-root = insert(root, 30)
-root = insert(root, 15)
-root = insert(root, 25)
-root = insert(root, 40)
-root = insert(root, 50)
-root = insert(root, 23)
-print("Inorder Traversal of the BST:")
-inorder(root)  # 15 20 23 25 30 40s
-    
+root = Node(20)
+root.left = Node(15)
+root.right = Node(30)
+root.right.left = Node(25)
+root.right.right = Node(40)
+root.right.left.left = Node(23)
+root.right.right.right = Node(50)
 
-# root = Node(20)
-# root.left = Node(15)
-# root.right = Node(30)
-# root.right.left = Node(25)
-# root.right.right = Node(40)
-# root.right.left.left = Node(23)
-# root.right.right.right = Node(50)
-    # print("Inorder Traversal of the BST:")
-    # root.inorder(root)  # 15 20 23 25 30 40
+print("Inorder Traversal of the BST:")
+inorder(root)  # 15 20 23 25 30 40 50
+
+print("\n\nInserting values into the BST:")
+insert(root, 10)
+inorder(root)  # 10 15 20 23 25 30 40 50
+
+print("\n\nDeleting values from the BST:")
+delete(root, 30)
+delete(root, 15)
+inorder(root)  # 10 20 23 25 40 50   
+
+print("\n\nSearching for values in the BST:")
+search(root, 25)  # Value found in the BST.
+search(root, 100)  # Value not found in the BST.    
+
